10e7ac08edfe5db78194036ca334261310c6a52d
George Waseem <94840552+geowaseem@users.noreply.github.com>
2024-02-04 12:44:38 +0200

    Create yolo.py

---
+++ yolo.py
@@ -0,0 +1,18 @@
+import os
+import random
+import requests
+import torch
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from PIL import Image
+from transformers import DetrForObjectDetection
+from ultralytics import YOLO, RTDETR
+import cv2
+
+model = YOLO('D:/.pt')
+
+results = model. predict(source="0", show=True)
+
+print(results)
